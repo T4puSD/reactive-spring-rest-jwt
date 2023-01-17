b4c9d27761aa9c5fc16b7e0d77d59c090f8df0f5
t4pusd <sdtapusd20@gmail.com>
2023-01-17 21:38:44 +0600

    init project

---
+++ src/main/java/com/tapusd/reactivespringrestjwt/ReactiveSpringRestJwtApplication.java
@@ -0,0 +1,13 @@
+package com.tapusd.reactivespringrestjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringRestJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringRestJwtApplication.class, args);
+	}
+
+}
