11228929d72eb1a573dbc42d94c67405beee5703
t4pusd <sdtapusd20@gmail.com>
2023-02-13 00:03:07 +0600

    refactored code

--- src/main/java/com/tapusd/reactivespringrestjwt/web/controller/user/MyAccountController.java
+++ src/main/java/com/tapusd/reactivespringrestjwt/web/controller/user/MyAccountController.java
@@ -1,15 +1,12 @@
 package com.tapusd.reactivespringrestjwt.web.controller.user;
 
-import com.tapusd.reactivespringrestjwt.domain.Account;
 import com.tapusd.reactivespringrestjwt.dto.AccountDTO;
+import com.tapusd.reactivespringrestjwt.util.AuthContextUtils;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1/my")
@@ -17,16 +14,8 @@ public class MyAccountController {
 
     @GetMapping("/info")
     public Mono<ResponseEntity<AccountDTO>> getMyInfo() {
-        return ReactiveSecurityContextHolder.getContext()
-                .map(context -> {
-                    var authentication = context.getAuthentication();
-                    var account = (Account) authentication.getPrincipal();
-                    if (Objects.isNull(account)) {
-                        throw new IllegalStateException("Not authenticated");
-                    }
-
-                    return ResponseEntity.ok(new AccountDTO(account.getUid().toHexString(),
-                            account.getEmail(), account.getRoles()));
-                });
+        return AuthContextUtils.getAccountFromAuthContext()
+                .map(account -> ResponseEntity.ok(new AccountDTO(account.getUid().toHexString(),
+                        account.getEmail(), account.getRoles())));
     }
 }
